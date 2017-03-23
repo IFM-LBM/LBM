7a6bfb9c8539e44f04657116e7ce4261d9499e2e
jagc123 <gonzalez@ifm.umich.mx>
2017-03-23 15:21:41 -0600

    mpi initialization

--- main.f90
+++ main.f90
@@ -5,7 +5,29 @@ program main
   use variables
   
   implicit none
-  write(*,*) "Starting LBM"
 
-  write(*,*) "Finishing LBM"
+  integer :: error
+  integer :: p
+  integer :: id
+  
+  !Initialize MPI
+  call MPI_Init(error)
+
+  !Get the number of processes.
+  call MPI_Comm_size ( MPI_COMM_WORLD, p, error )
+
+  !Get the individual process ID.
+  call MPI_Comm_rank ( MPI_COMM_WORLD, id, error )
+
+  if (id==0) then
+     write(*,*) "Starting LBM"
+  endif
+
+  if (id==0) then
+     write(*,*) "Finishing LBM"
+  endif
+  
+  !Finalize MPI
+  call MPI_Finalize(error)
+
 end program main
