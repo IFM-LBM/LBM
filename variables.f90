8b231df772550f1ff94e4910a95d290002954b60
Guillermo Estrada <gaeg92@gmail.com>
2017-03-28 03:14:31 -0600

    Update variables.f90

--- variables.f90
+++ variables.f90
@@ -2,6 +2,9 @@ module variables
 
   implicit none
 
-  real(8), allocatable :: x(:)
+  real(8),allocatable,dimension(:)::x,T,w
+  real(8),allocatable,dimension(:,:)::f,feq,omega,f_new
+  integer n,time,vel,i,j 
+  real(8) alfa, womega, tiempo
   
 end module variables
