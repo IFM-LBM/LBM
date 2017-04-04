f6594c7bd94089f015ee2a83c606e4ecf9e9452b
jagc123 <gonzalez@ifm.umich.mx>
2017-04-04 09:53:03 -0500

    changing n -> nx_total

--- allocate.f90
+++ allocate.f90
@@ -6,8 +6,8 @@ subroutine allocate
 
   implicit none
 
-  allocate(x(0:n),T(0:n),w(1:vel))
-  allocate(f(1:vel,0:n),f_new(1:vel,0:n))
-  allocate(feq(1:vel,0:n))
+  allocate(x(0:Nx_total),T(0:Nx_total),w(1:vel))
+  allocate(f(1:vel,0:Nx_total),f_new(1:vel,0:Nx_total))
+  allocate(feq(1:vel,0:Nx_total))
   
 end subroutine allocate
--- parameters.f90
+++ parameters.f90
@@ -2,7 +2,7 @@ module parameters
 
   implicit none
   
-  integer, parameter :: n = 100
+  integer, parameter :: Nx_total = 100
   integer, parameter :: time = 200
   integer, parameter :: vel = 2 
   real(8), parameter :: alfa = 0.25d0
